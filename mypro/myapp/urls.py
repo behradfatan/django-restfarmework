85e5e0016e450e49bea44bd9b169afee267aef49
behradfatan <behrad_fatan@yahoo.com>
2021-10-01 21:11:26 +0330

    create a model and serializers and show person's data with show-person in url

--- mypro/myapp/urls.py
+++ mypro/myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello', views.hello),
-    path('welcome', views.welcome),
+    path('hello/', views.hello),
+    path('welcome/', views.welcome),
+    path('show-person/', views.show_person),
 ]
--- mypro/myapp/views.py
+++ mypro/myapp/views.py
@@ -1,5 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Person
+from .serializers import PersonSerializer
 
 @api_view()
 def hello(request):
@@ -13,3 +15,10 @@ def welcome(request):
         return Response({"message": f' welcome {name}'})
     else:
         return Response({"message": "welcome"})
+
+
+@api_view()
+def show_person(request):
+    person = Person.objects.all()
+    ser = PersonSerializer(person, many=True)
+    return Response(ser.data)
