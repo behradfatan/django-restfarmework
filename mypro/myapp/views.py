9741f20d1e368a47b51612e190b328694d568e59
behradfatan <behrad_fatan@yahoo.com>
2021-10-01 20:30:01 +0330

    create a api with get and post method (get name and welcome with name)

--- mypro/myapp/views.py
+++ mypro/myapp/views.py
@@ -4,3 +4,12 @@ from rest_framework.decorators import api_view
 @api_view()
 def hello(request):
     return Response({"message": "welcome"})
+
+
+@api_view(["post", "GET"])
+def welcome(request):
+    if request.method == "POST":
+        name = request.data["name"]
+        return Response({"message": f' welcome {name}'})
+    else:
+        return Response({"message": "welcome"})
